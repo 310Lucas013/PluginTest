a632b10033c116fc3833f8f1882231fcc7849ca3
Vermeer,Luuk L.L.H <l.vermeer@student.fontys.nl>
2020-03-31 22:35:22 +0200

    Working stuff and things

--- src/main/java/luc/plugin/command/TestCommand.java
+++ src/main/java/luc/plugin/command/TestCommand.java
@@ -1,6 +1,7 @@
 package luc.plugin.command;
 
 import luc.plugin.Main;
+import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -12,7 +13,7 @@ public class TestCommand implements CommandExecutor {
     @Override
     public boolean onCommand(final CommandSender sender, Command command, String s, final String[] args) {
         main.getServer().getScheduler().runTaskAsynchronously(main, () -> {
-            System.out.println("idk");
+            Bukkit.broadcastMessage("Test Message");
         });
         return true;
     }
